cbd3d2db7b57fbbba46c08c6615e12db27441f5b
kbaikov <konstantin.baikov@gmail.com>
2020-12-10 18:58:18 +0100

    Solve day 9

---
+++ day09.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from typing import Iterable, Optional
+import itertools
+
+import pytest
+
+test = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"""
+
+
+def wrong_number(f: str, current) -> int:
+    lookbehind = 0
+    nums = [int(x) for x in f.split()]
+    while True:
+        if not any(
+            x + y == nums[current]
+            for x, y in list(itertools.combinations(nums[lookbehind:current], 2))
+        ):
+            return nums[current]
+        current += 1
+        lookbehind += 1
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    (
+        ((test, 5), 127),
+        ((Path("input09.txt").read_text().strip(), 25), 177777905),
+    ),
+)
+def test_wrong_number(input, expected) -> None:
+
+    assert wrong_number(*input) == expected
+
+
+def long_sum(f: str, number) -> Optional[int]:
+    start = 0
+    end = 0
+    nums = [int(x) for x in f.split()]
+    for start in range(len(f)):
+        for end in range(start + 1, len(f)):
+            l = nums[start : end + 1]
+            s = sum(l)
+            if s == number:
+                return min(l) + max(l)
+            elif s > number:
+                break
+    return None
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    (
+        ((test, 127), 62),
+        ((Path("input09.txt").read_text().strip(), 177777905), 23463012),
+    ),
+)
+def test_long_sum(input, expected) -> None:
+
+    assert long_sum(*input) == expected
+
+
+if __name__ == "__main__":
+
+    f = Path("input09.txt").read_text().strip()
+    print(wrong_number(f, 25))
+    print(long_sum(f, 177777905))
