40abc6c915d569da477846a809261bbcd71d4608
kbaikov <konstantin.baikov@gmail.com>
2021-09-16 08:01:49 +0200

    Add download function
    Reads a cookie from .env

---
+++ download.py
@@ -0,0 +1,18 @@
+import urllib.request
+import argparse
+from pathlib import Path
+
+content = Path(".env").read_text().strip()
+
+
+def get_input(day):
+    url = f"https://adventofcode.com/2020/day/{day}/input"
+    req = urllib.request.Request(url, headers={"Cookie": content})
+    return urllib.request.urlopen(req).read().decode()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("day", type=int)
+    args = parser.parse_args()
+    Path(f"input{args.day:02}.txt").write_text(get_input(args.day))
